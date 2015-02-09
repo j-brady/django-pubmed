9e077845ddd16db9ddcf070a80f6e2be0e0a72d1
Jacob Brady <jacobbrady@Jacob-Bradys-MacBook-Pro.local>
2015-02-09 11:55:45 +0000

    improved filter and added delete all pubs without deleting search term.

--- publications/admin.py
+++ publications/admin.py
@@ -17,13 +17,19 @@ def get_pubmed_entry(modeladmin,request,queryset):
       a = Publication(authors=author_list,year=year_temp,title=title_temp,journal=journal_temp,volume_issue_pages='',search_term=search_term,pmid=pmid_temp)
       # Save changes to the database if entry is unique - This is very inefficient though
       check_unique(new_publication=a)
+
+def delete_publications_from_list(modeladmin,request,queryset):
+  for p in queryset:
+    search_term = p.search_term
+    Publication.objects.filter(search_term=search_term).delete()
+    
       
 def check_unique(new_publication):
   # Function to check that entries are unique
-  # Need to filter against search terms to.
+  # filters against (title, search_term) tuple.
   pubs   = Publication.objects.all()
-  titles = [pub.title for pub in pubs]
-  if new_publication.title in titles:
+  titles = [(pub.title,pub.search_term) for pub in pubs]
+  if (new_publication.title,new_publication.search_term) in titles:
     pass
   else:
     new_publication.save()
@@ -34,11 +40,11 @@ def check_unique(new_publication):
 get_pubmed_entry.short_description = "Fetch Pubmed entry"
 
 class PublicationAdmin(admin.ModelAdmin):
-  list_display = ('authors', 'year')
+  list_display = ('authors', 'year','search_term')
 
 class SearchTermAdmin(admin.ModelAdmin):
   list_display = ('search_name','search_term')
-  actions = [get_pubmed_entry] 
+  actions = [get_pubmed_entry,delete_publications_from_list] 
 
 admin.site.register(Publication,PublicationAdmin)
 admin.site.register(SearchTerm,SearchTermAdmin)
